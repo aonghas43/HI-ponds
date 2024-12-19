56978285c9561d3fbb8bc8c1a8b2c27d4e2ed42d
aonghas43 <mrmoose.macaulay@gmail.com>
2024-12-19 11:19:36 +0000

    initial version with base maps

--- app/data/ponds.js
+++ app/data/ponds.js
@@ -1,2 +1,6 @@
-use strict;
-var ponds = {}
+var ponds = {
+    "type": "FeatureCollection",
+"name": "HIGS 2024-25 4 Ponds",
+"description": "HIGS 2024-25 Ponds Survey",
+"features": [ ]
+}
--- app/scripts/mapscript.js
+++ app/scripts/mapscript.js
@@ -1,70 +1,68 @@
-<!-- map code here -->
+//<!-- map code here -->
 {
-	 "use strict";
+	"use strict";
 		 
 	// base maps
 	    var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 						maxZoom: 20,
-						attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+						attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+						crossOrigin: 'anonymous'
 					});
-       const key = 'pRA400aOhw0IeiIEAtBt';
-		 
-		var maps1888 = L.tileLayer(`https://api.maptiler.com/tiles/uk-osgb10k1888/{z}/{x}/{y}.jpg?key=pRA400aOhw0IeiIEAtBt`,
-		{ //style URL
-        tileSize: 512,
-        zoomOffset: -1,
-        minZoom: 1,
-        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-        crossOrigin: true}
-		);
+     
 		
-		var maps1900 = L.tileLayer(`https://api.maptiler.com/tiles/uk-osgb1888/{z}/{x}/{y}?key=pRA400aOhw0IeiIEAtBt`,
-		{ //style URL
-        tileSize: 512,
-        zoomOffset: -1,
-        minZoom: 1,
-        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-        crossOrigin: true}
-		);
-		
-		var maps1950= L.tileLayer(`https://api.maptiler.com/tiles/uk-osgb25k1937/{z}/{x}/{y}.jpg?key=pRA400aOhw0IeiIEAtBt`,
-		{ //style URL
-        tileSize: 512,
-        zoomOffset: -1,
-        minZoom: 1,
-        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-        crossOrigin: true}
-		);
-		
-		var landranger = L.tileLayer(`https://api.maptiler.com/tiles/uk-osgb63k1955/{z}/{x}/{y}.jpg?key=pRA400aOhw0IeiIEAtBt`,
-		{ //style URL
-        tileSize: 512,
-        zoomOffset: -1,
-        minZoom: 1,
-        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-        crossOrigin: true}
-		);
+	var uk10_1949_1972 = L.tileLayer('https://geo.nls.uk/mapdata3/os/britain10knationalgridnew/{z}/{x}/{y}.png', {
+		maxZoom: 20,
+		opacity: 0.85,
+		crossOrigin: 'anonymous',
+		attribution: 'Reproduced with the permission of the National Library of Scotland \
+		               <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY</a>'
+	});
+
+	var uk10_1830_1880 = L.tileLayer('https://mapseries-tilesets.s3.amazonaws.com/os/six-inch-cambridgeshire/{z}/{x}/{y}.png',{
+		maxZoom: 20,
+		opacity: 0.85,
+		crossOrigin: 'anonymous',
+		attribution: 'Reproduced with the permission of the National Library of Scotland \
+		               <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY</a>'
+	});
+
+
+	const key = 'pRA400aOhw0IeiIEAtBt';
+
+	uk1880_1913 = L.tileLayer('https://api.maptiler.com/tiles/uk-osgb10k1888/{z}/{x}/{y}.jpg?key=' + key, {
+		maxZoom: 17,
+		opacity: 0.85,
+		crossOrigin: 'anonymous',
+		attribution: '\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\  \
+					Reproduced with the permission of the National Library of Scotland \
+		               <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY</a>'
+	});
+
+	var pondsLayer = L.geoJSON(ponds,{
+			attribution: 'Ponds data owned on behalf of the community by \
+			<a href="https://www.higreenspaces.org/about-us">Histon and Impington Green Spaces</a>'
+		});
+
 };
 
 
 		// set up map
-		var HisImp = [52.2500, 0.1042];
+		var HisImp = [52.253815589039775, 0.10777473100461067];
 		var map = L.map('map', {
 			center: HisImp,
-			zoom: 15,
+			zoom: 14,
 			// layers which are on by default
 			layers: [tiles]
 			});		
 		// add Layer Control
 		var baseMaps = { 
 						"OpenStreetMap" : tiles,
-						"1888 OS" : maps1888,
-						"1900 OS": maps1900,
-						"1950 OS" : maps1950,
-						"Landranger 7 1955" : landranger
+						"OS 1:10,000/1:10,560, Great Britain, 1949-1972": uk10_1949_1972,
+						"OS six-Inch 1880-1915": uk1880_1913,
+						"OS six-Inch 1830-1880": uk10_1830_1880
   					}
 		// available layers
-		var overLays = { "ponds : ponds
+		var overLays = { "ponds" : pondsLayer
 						}
 	
         var layerControl = L.control.layers(baseMaps, overLays).addTo(map);
